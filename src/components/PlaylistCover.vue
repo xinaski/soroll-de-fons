e0bf843a1c857115ac319ae3bcbe51b92c9bc4e0
xinaski <xinasking@gmail.com>
2026-09-20 16:23:46 +0200

    Add src/components/PlaylistCover.vue

---
+++ src/components/PlaylistCover.vue
@@ -0,0 +1,10 @@
+<template>
+  <figure class="cover">
+    <img :src="playlist.cover" :alt="playlist.coverAlt" />
+    <figcaption><span>{{ playlist.title }}</span><small>SDF · 001</small></figcaption>
+  </figure>
+</template>
+
+<script setup>
+defineProps({ playlist: { type: Object, required: true } })
+</script>
