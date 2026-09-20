e11e13a5cd73efa32d3adec2bf5e2bcdda738887
xinaski <xinasking@gmail.com>
2026-09-20 16:23:54 +0200

    Add src/components/SiteHeader.vue

---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,8 @@
+<template>
+  <header class="site-header">
+    <RouterLink to="/" class="brand" aria-label="Soroll de fons, inici">
+      <span>Soroll de fons</span><small>Històries explicades cançó a cançó.</small>
+    </RouterLink>
+    <span class="issue">Arxiu sonor · 001</span>
+  </header>
+</template>
